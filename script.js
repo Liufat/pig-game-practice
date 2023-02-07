9324dc82579e04484a4f5a91d6849d02feb8626b
Liufat <dd5672003@gmail.com>
2023-02-07 12:29:41 +0800

    [新增]切換玩家的功能

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 // --------選擇器----------
 const score0 = document.querySelector('#score--0');
 const score1 = document.querySelector('#score--1');
+const player0 = document.querySelector('.player--0');
+const player1 = document.querySelector('.player--1');
 const current0 = document.querySelector('#current--0');
 const current1 = document.querySelector('#current--1');
 const dice = document.querySelector('.dice');
@@ -16,7 +18,11 @@ dice.classList.add('hidden');
 
 // --------搖骰子功能------
 
-let currentScore = 0;
+let activePlayer = 0; //當前玩家
+
+let currentScore = 0; //當前分數
+
+let scores = [0, 0]; //紀錄兩名玩家總分的array
 
 btnRoll.addEventListener('click', () => {
   // 1.產生一個1~6的隨機數字
@@ -30,9 +36,15 @@ btnRoll.addEventListener('click', () => {
   if (diceNumber !== 1) {
     // 3-1.如果不是1，將數字加到當前分數內
     currentScore += diceNumber;
-    current0.textContent = currentScore; //待修改
+    document.querySelector(`#current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // 3-2.如果是1，將當前分數清空並切換玩家
     currentScore = 0;
+    document.querySelector(`#current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    // 切換玩家的視覺效果
+    player0.classList.toggle('player--active');
+    player1.classList.toggle('player--active');
   }
 });
